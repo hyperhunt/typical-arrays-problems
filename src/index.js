0799841a281db5e5eba93a6161cb78304095b0c5
Tony Branch <tonybranch.org@yahoo.com>
2020-10-03 16:49:49 +0300

    Done task: typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+exports.min = function min(array) {
+    return !Array.isArray(array) || !array.length
+        ? 0
+        : Math.min.apply(this, array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return !Array.isArray(array) || !array.length
+        ? 0
+        : array.reduce((acc, cur) => {
+              return acc > cur ? acc : cur;
+          });
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    // return !array ? 0 : array.reduce((a, b) => a + b) / array.length;
+    // return !array ? 0 : eval(array.join('+')) / array.length; // eval is evil
+    return !Array.isArray(array) || !array.length
+        ? 0
+        : (function () {
+              let res = 0;
+              for (const i in array) {
+                  res += array[i];
+              }
+              return res;
+          })(array) / array.length;
+};
